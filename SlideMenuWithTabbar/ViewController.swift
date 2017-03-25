e5a47a5d75c32d5b3a1368ba852e6ebe182e374d
Chun-Tang Wang <digdoritos@gmail.com>
2017-03-26 00:57:16 +0800

    Initial Commit

---
+++ SlideMenuWithTabbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMenuWithTabbar
+//
+//  Created by Chun-Tang Wang on 26/03/2017.
+//  Copyright © 2017 Chun-Tang Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
